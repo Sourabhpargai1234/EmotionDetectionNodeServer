4c5ed6b76f25afc308efd0537cdcbe92c179e10f
sourabh.pargai <sourabhpargai1234@gmail.com>
2025-04-23 22:30:45 +0530

    health commit

--- api/index.js
+++ api/index.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok', message: 'Server is healthy 🚀' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running locally at http://localhost:${PORT}`);
 });
